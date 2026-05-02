6c9c780afce81f3b0f851cc3e6a3935d1db82c69
MiKiNuo <luojiji110@foxmail.com>
2026-05-03 04:44:38 +0800

    删除旧的项目

--- MVI/Demo/Unity/MVIDemo/Assets/Scripts/MviResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MVI.Demo
-{
-    public record MviResult :IMviResult
-    {
-        public int Code { set; get; }
-        public string Message { set; get; }
-        public object Data { set; get; }
-    }
-}
--- MVI/MVI/src/IState.cs
+++
@@ -1,7 +0,0 @@
-﻿
-namespace MVI
-{
-    public interface IState
-    {
-    }
-}
